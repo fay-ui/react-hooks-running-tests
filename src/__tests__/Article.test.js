5bb5a7b2753a67e37ff63b40fccae0ce2102507f
faith wangari njau <faithjonnes91@gmail.com>
2024-11-01 11:34:18 +0300

    add text

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,12 @@
+// src/__tests__/Article.test.js
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import Article from "../components/Article"; // Adjust the path as needed
 
-import Article from "../components/Article";
-
+// Your test case
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  screen.debug(); // This will log the current state of the DOM
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
